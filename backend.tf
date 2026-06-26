0331fcc5cca12d3b5199664e8acd2bcb121ced15
Premkumar <nkprembe@gmail.com>
2026-06-26 21:39:30 +0530

    modified backend.tf file

--- backend.tf
+++ backend.tf
@@ -1,16 +1,8 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "myapp-prod-tf-state-remote-backend"
-  tags = {
-    Name        = "Terraform Remote State"
-    Environment = "prod"
-    ManagedBy   = "Terraform"
-  }
-}
-
-resource "aws_s3_bucket_versioning" "tf_state" {
-  bucket = aws_s3_bucket.tf_state.id              # ← reference, not hardcoded name
-
-  versioning_configuration {
-    status = "Enabled"
+terraform {
+  backend "s3" {
+    bucket         = "myapp-prod-tf-state-remote-backend"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
   }
 }
