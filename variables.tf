2fc808f7921aa98dbfb955426132bf70634271b8
Premkumar <nkprembe@gmail.com>
2026-06-27 11:21:21 +0530

    adding proper AZ

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "private_subnet_cidrs" {
