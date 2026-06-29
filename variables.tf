3529338271732a8c97165e18b1e5f959c42c746c
Premkumar Nagaiah <138997281+PremN-IT@users.noreply.github.com>
2026-06-30 00:12:28 +0530

    Change default instance type from t2.medium to t3.small

--- variables.tf
+++ variables.tf
@@ -59,7 +59,7 @@ variable "node_groups" {
   }))
   default = {
     general = {
-      instance_types = ["t2.medium"]
+      instance_types = ["t3.small"]
       capacity_type  = "ON_DEMAND"
       scaling_config = {
         desired_size = 2
